c5cbd7f6203d92dd5b4eeaac6f4dafd8c9f74d21
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-18 18:06:24 +0530

    Tip Calculator Basic Functionality

--- Tip-Calculator/src/App.jsx
+++ Tip-Calculator/src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function TipCalculator(){
+
+  const [billAmount, setBillAmount] = useState(0);
+  const [TipPercentage, setTipPercentage] = useState(0);
+  const [tip, setTip] = useState(0);
+
+  function BillAmount(e){
+    let Bill = e.target.value;
+    calculateTip(Bill, tipPercentage);
+    setBillAmount(Bill);
+  }
+  function tipPercentage(e){
+    let TipP = e.target.value;
+    calculateTip(TipP);
+    setTipPercentage(BillAmount, TipP);
+  }
+
+  function calculateTip(Bill, TipP){
+    const tip = (Bill * TipP) / 100;
+    setTip(tip);
+  }
+
+
+  return (
+    <div>
+      <h1>Tip Calculator</h1>
+      <input type="number" placeholder='Enter Bill Amount' onChange={BillAmount}/>
+      <input type="number" placeholder='Tip Percentage' onChange={tipPercentage}/>
+      <h2>Bill Amount : {billAmount}</h2>
+      <h2>Tip Percentage : {TipPercentage}</h2>
+      <h1>Total Bill : {tip.parseFloat().Fixed(2)}</h1>
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TipCalculator />
     </>
   )
 }
