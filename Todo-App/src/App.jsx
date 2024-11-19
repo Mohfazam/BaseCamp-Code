54ed866f0bc1ec1c4b2b6c6368d0568c3b1e32f6
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-20 01:53:41 +0530

    Todo List

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+
+function TodoItem(){
+  const [todo, setTodo] = useState({});
+
+  function addTodo(e){
+    const todoDes = e.target.value;
+    setTodo({...todo, todoDes});
+  }
+  return(
+    <div>
+      <p>Todo List</p>
+      <input type="text" placeholder='Enter a task' onChange = {addTodo}/>
+      <button >Add Task</button>
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoItem />
+      
     </>
   )
 }
