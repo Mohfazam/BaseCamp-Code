7381072bff778a66f8b4244bb9e661f61a22af69
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-20 02:01:44 +0530

    Add Todo Functionality

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
 
-import './App.css'
+function TodoItem() {
+  const [todo, setTodo] = useState(''); // To hold the input value
+  const [todos, setTodos] = useState([]); // To hold the list of tasks
 
-
-function TodoItem(){
-  const [todo, setTodo] = useState({});
-
-  function addTodo(e){
-    const todoDes = e.target.value;
-    setTodo({...todo, todoDes});
+  // Update input value
+  function handleInputChange(e) {
+    setTodo(e.target.value);
   }
-  return(
+
+  // Add a task to the list
+  function addTodo() {
+    if (todo.trim()) {
+      setTodos([...todos, todo]); // Append task to list
+      setTodo(''); // Clear input field
+    }
+  }
+
+  return (
     <div>
       <p>Todo List</p>
-      <input type="text" placeholder='Enter a task' onChange = {addTodo}/>
-      <button >Add Task</button>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={handleInputChange}
+      />
+      <button onClick={addTodo}>Add Task</button>
+
+      <ul>
+        {todos.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 function App() {
-  
-
   return (
     <>
       <TodoItem />
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
