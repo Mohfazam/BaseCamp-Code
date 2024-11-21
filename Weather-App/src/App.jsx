4ca96176a5b1b148c06657b494d011b413d6636f
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-21 20:32:03 +0530

    Weather App

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -1,35 +1,102 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios';
+import dotenv from 'dotenv';
+const apiKey = import.meta.env.WEATHER-API;
+
+dotenv.config();
+
+
 import './App.css'
+import { set } from 'mongoose';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Weather App</h1>
+      <Fetchweather />
+    </>
+  )
+
+}
+
+
+function Fetchweather(){
+
+  const [city, setCity] = useState("");
+  const [Lat, setLat] = useState(null);
+    const [Lon, setLon] = useState(null);
+    const [main, setmain] = useState("");
+    const [des, setdes] = useState("");
+
+  function handleSearch(e){
+    setCity(e.target.value);
+  }
+
+
+  async function handleSubmit(e) {
+    
+    e.preventDefault();
+    try {
+      const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct`, {
+        params: {
+          q: city,
+          limit: 1,
+          appid: WEATHER-API,
+        },
+      });
+      if(response.data.length > 0){
+        const location = response.data[0];
+        setLat(location.lat);
+        setLon(location.lon);
+        fetchData(location.lat, location.lon);
+      }
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  }
+
+  async function fetchData(lat, lon){
+    try{
+      const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+        params: {
+          lat: lat,
+          lon: lon,
+          appid: WEATHER-API,
+          units: "metric"
+        }
+      });
+      
+
+      
+        setmain(weather.data.weather[0].main);
+        setdes(weather.data.weather[0].description);
+        console.log(weather.data.weather[0].main);
+        console.log(weather.data.weather[0].description);
+      
+    } catch(error){
+      console.log("Error fetching weather data:", error);
+    }
+  }
+  
+  
+
+  return (
+    <>
+      <input type="text" placeholder="Enter Your City" onChange={handleSearch}/>
+      <button onClick={handleSubmit}>Search</button>
+      <h2>Weather in {city}</h2>
+      <p>Main Weather: {main}</p>
+      <p>Description: {des}</p>
+
     </>
   )
 }
+
+
+
 
 export default App
