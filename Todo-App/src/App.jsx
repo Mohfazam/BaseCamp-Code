367a0bbfe3f10d8105b0d888e25d0a8780057178
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-21 13:49:56 +0530

    Check Functionality

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -5,9 +5,21 @@ function App() {
   const [listTodo, setlistTodo] = useState([]);
 
   function addTodo(text) {
-    if(text !== ""){
-      setlistTodo([...listTodo, text]);
+    if (text !== "") {
+      setlistTodo([...listTodo, { text, completed: false }]);
     }
+  }
+
+  function toggleComplete(index) {
+    const newList = [...listTodo];
+    newList[index].completed = !newList[index].completed;
+    setlistTodo(newList);
+  }
+
+  function deleteItem(key) {
+    const newList = [...listTodo];
+    newList.splice(key, 1);
+    setlistTodo(newList);
   }
 
   return (
@@ -17,31 +29,28 @@ function App() {
           <TodoInput addTodo={addTodo} />
           <div className="app-heading">Todo List</div>
           <hr />
-          {listTodo.map((listItem, i) => {
-            return <TodoList item={listItem} index={i} key={i} deleteItem = {() => deleteItem(i)}/>;
-          })}
+          {listTodo.map((listItem, i) => (
+            <TodoList
+              item={listItem}
+              index={i}
+              key={i}
+              deleteItem={() => deleteItem(i)}
+              toggleComplete={() => toggleComplete(i)}
+            />
+          ))}
         </div>
       </div>
     </>
   );
-
-
-
-
-  function deleteItem(key){
-    let newList = [...listTodo];
-    newList.splice(key, 1);
-    setlistTodo(newList);
-  }
 }
 
 function TodoInput({ addTodo }) {
   const [inputText, setInputText] = useState("");
 
   function handleEnterFunctionality(e) {
-    if(e.key === "Enter"){
+    if (e.key === "Enter") {
       addTodo(inputText);
-          setInputText("");
+      setInputText("");
     }
   }
 
@@ -55,7 +64,6 @@ function TodoInput({ addTodo }) {
         onChange={(e) => setInputText(e.target.value)}
         onKeyDown={handleEnterFunctionality}
       />
-
       <button
         className="add-btn"
         onClick={() => {
@@ -69,20 +77,21 @@ function TodoInput({ addTodo }) {
   );
 }
 
-function TodoList(props) {
+function TodoList({ item, deleteItem, toggleComplete }) {
   return (
-    <div className="list-item">
-      {props.item}
+    <div
+      className="list-item"
+      style={{
+        textDecoration: item.completed ? "line-through" : "none",
+      }}
+    >
+      {item.text}
       <span className="icons">
-        {/* <i class="fa-solid fa-trash icon-delete" onClick={(e) => {
-          // props.deleteItem(index);
-        }}></i> */}
-        <i className="fa-solid fa-trash icon-delete" onClick={() => props.deleteItem(props.index)}></i>
+      <i className="fa-solid fa-check" id="check-icon" onClick={toggleComplete}></i>
+      <i className="fa-solid fa-trash icon-delete" onClick={deleteItem}></i>
       </span>
     </div>
   );
 }
-
-
 
 export default App;
